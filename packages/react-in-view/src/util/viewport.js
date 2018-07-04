3187cdaaf1c9686fc67f37acf2449a9ea5647b85
Benjamin Huang <ben.huang06@gmail.com>
2018-07-04 14:03:33 -0400

    Add viewport functions

---
+++ packages/react-in-view/src/util/viewport.js
@@ -0,0 +1,9 @@
+const viewportHeight = () =>
+    window.innerHeight || document.documentElement.clientHeight;
+
+const viewportWidth = () =>
+    window.innerWidth || document.documentElement.clientWidth;
+
+export {
+    viewportHeight, viewportWidth
+};
---
+++ packages/react-in-view/src/util/viewportEvaluators.js
@@ -0,0 +1,40 @@
+import { viewportHeight, viewportWidth } from "./viewport";
+
+/**
+ * Factory for a function that evaluates a DOM element's position
+ * in the viewport.
+ */
+const createViewportEvaluator = (viewportDimension, upperValue, lowerValue) =>
+    element => {
+        if (!element) {
+            return false;
+        }
+
+        const rect = element.getBoundingClientRect();
+        const upper = rect[upperValue];
+        const lower = rect[lowerValue];
+
+        const inView = (upper >= 0 || lower >= 0) &&
+            (upper < viewportDimension || lower < viewportDimension);
+        const completelyInView = upper >= 0 && lower >= 0 &&
+            upper < viewportDimension && lower < viewportDimension;
+
+        return {
+            inView,
+            completelyInView
+        };
+    };
+
+const inViewportHeight = createViewportEvaluator(
+    viewportHeight(),
+    "top",
+    "bottom"
+);
+
+const inViewportWidth = createViewportEvaluator(
+    viewportWidth(),
+    "left",
+    "right"
+);
+
+export { inViewportHeight, inViewportWidth };
