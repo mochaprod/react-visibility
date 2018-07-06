3d880567f06656d5f20a2438f9839d1ed295b5fa
Benjamin Huang <ben.huang06@gmail.com>
2018-07-06 10:57:56 -0400

    Add simple styles and status box

--- packages/docs/src/App.js
+++ packages/docs/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Test from "./components/Test";
+import Test from "./components/demo/Test";
+import "./App.scss";
 
 ReactDOM.render(
     <Test />,
