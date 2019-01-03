0da72d115262aeb4081b5a3978133540c075d647
Benjamin Huang <ben.huang06@gmail.com>
2019-01-03 17:00:50 -0500

    Export `ScrollingState`

--- packages/react-visibility/src/index.js
+++ packages/react-visibility/src/index.js
@@ -1,3 +1,4 @@
 export { default as InView } from "./components/InView";
 export { default as withInView } from "./components/withInView";
 export { default as WindowScroll } from "./components/WindowScroll";
+export { default as ScrollingState } from "./components/Scrolling/ScrollingState";
