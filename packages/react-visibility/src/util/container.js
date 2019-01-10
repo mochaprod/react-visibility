8148a54dd27e27ff0649661ba15dceafaeb90e67
Benjamin Huang <ben.huang06@gmail.com>
2019-01-09 23:55:06 -0500

    Additional functions for elements
    Also add `container` argument for viewport functions

--- packages/react-visibility/src/util/container.js
+++ packages/react-visibility/src/util/container.js
@@ -1,23 +1,81 @@
 import { canUseDOM, DOCUMENT_ELEMENT } from "./env";
 
+// If `container === window`, then use `window.pageYOffset`
+// otherwise, use `container.scrollTop`.
+// If `window`, use `window.innerHeight`; or use
+// `container.offsetHeight`.
+// If `window` use `DOC.body.scrollHeight`,
+// `DOC.documentElement.scrollHeight`; or use `container.scrollHeight`.
+
+// If `window`, use `getBoundingClientRect()`; or use
+// custom `offsetParent` method like `gBCR()`.
+
 function getEventTarget(element) {
     return element.documentElement || element;
 }
 
-function pollClientDimensions(element = DOCUMENT_ELEMENT) {
+/**
+ * Computes the height and width of the content of an element
+ * that excludes scrollbars and borders.
+ * Uses the `clientWidth` and `clientHeight` dimensions.
+ *
+ * @param {window | HTMLElement} element obtain dimensions from
+ * @param {boolean} total `true` to use offset dimensions
+ */
+function pollClientDimensions(element = window, total = false) {
+    if (element === window) {
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    }
+
     // Includes element height and padding, no borders/margins!
     const {
         clientWidth,
-        clientHeight
+        clientHeight,
+        offsetWidth,
+        offsetHeight
     } = element;
 
     return {
-        width: clientWidth,
-        height: clientHeight
+        width: total
+            ? offsetWidth
+            : clientWidth,
+        height: total
+            ? offsetHeight
+            : clientHeight
     };
 }
 
-function pollContainerScrollProperties(element = DOCUMENT_ELEMENT) {
+/**
+ * Computes the scrolling properties of an element.
+ * Includes:
+ *  * `scrollTop` - total height scrolled
+ *  * `scrollLeft` - total width scrolled
+ *  * `scrollWidth` - total available width to scroll
+ *  * `scrollHeight` - total available height to scroll
+ *
+ * @param {*} element
+ */
+function pollContainerScrollProperties(element = window) {
+    if (element === window) {
+        // `window` can scroll across the entire `document`
+
+        return {
+            scrollTop: window.pageYOffset,
+            scrollLeft: window.pageXOffset,
+            scrollWidth: Math.max(
+                document.body.scrollWidth,
+                DOCUMENT_ELEMENT.scrollWidth
+            ),
+            scrollHeight: Math.max(
+                document.body.scrollHeight,
+                DOCUMENT_ELEMENT.scrollWidth
+            )
+        };
+    }
+
     const {
         scrollTop,
         scrollLeft,
@@ -33,7 +91,7 @@ function pollContainerScrollProperties(element = DOCUMENT_ELEMENT) {
     };
 }
 
-function pollScrollingState(element = DOCUMENT_ELEMENT) {
+function pollScrollingState(element = window) {
     if (!canUseDOM) {
         return null;
     }
@@ -62,9 +120,64 @@ function pollScrollingState(element = DOCUMENT_ELEMENT) {
     };
 }
 
+/**
+ * Computes the 'bounding client rect' for any element relative
+ * to its `offsetParent` container. If the container is `window`,
+ * then simply `gBCR()` is used.
+ *
+ * @param {Element | HTMLElement} element an element
+ * @param {Window | Element} container a container element
+ */
+function getElementRect(element, container = window) {
+    // `offsetWidth` and `offsetHeight` give the full width and height
+    // including scrollbars.
+    if (container === window) {
+        return element.getBoundingClientRect();
+    }
+
+    const {
+        offsetTop,
+        offsetLeft,
+        offsetWidth,
+        offsetHeight,
+        offsetParent: parent
+    } = element;
+
+    const {
+        width: parentWidth,
+        height: parentHeight
+    } = pollClientDimensions(parent);
+
+    const right = parentWidth - offsetLeft - offsetWidth;
+    const bottom = parentHeight - offsetTop - offsetHeight;
+
+    const result = {
+        top: offsetTop,
+        left: offsetLeft,
+        bottom,
+        right
+    };
+
+    console.log(result);
+
+    return result;
+}
+
+/**
+ * Computes how far an element is from the very top of its
+ * `offsetParent`. If the container is `window` that would be
+ * how far the element from the top of the `document`.
+ *
+ * @param {HTMLElement} element
+ * @param {Window | HTMLElement} container
+ */
+function getScrollDistance(element, container = window) {
+}
+
 export {
     getEventTarget,
     pollClientDimensions,
     pollContainerScrollProperties,
-    pollScrollingState
+    pollScrollingState,
+    getElementRect
 };
